0f2c220882fe98993ff14fd690880602b18f8d52
Zephrio <76012925+CD-Chief@users.noreply.github.com>
2024-08-29 20:53:05 +0200

    Add functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function(){
+  return(
+    <Counter/>
+  )
 }
 
-export default App;
+function Counter(){
+  return(
+    <div>
+      <title> Counter  App </title>
+      <h1 style={{textAlign: "center"}}> Counter </h1>
+      <CounterBox/>
+    </div>
+  )
+}
+
+function CounterBox(){
+  const [count, setCount] = useState(0);
+
+  return(
+    <div style={{display: "flex"}}>
+      <CounterButtons count={count} setCount={setCount}/>
+      <CustomAdder count={count} setCount={setCount}/>
+    </div>
+  )
+}
+
+function CounterButtons({count, setCount}){
+
+  function HandleClick(amount){
+    setCount(count + amount);
+  }
+
+  return(
+    <form>
+      <button type='button' onClick={() => HandleClick(-10)}>-10</button>
+      <button type='button' onClick={() => HandleClick(-1)}>-1</button>
+
+      <h2>{count}</h2>
+
+      <button type='button' onClick={() => HandleClick(+10)}>+10</button>
+      <button type='button' onClick={() => HandleClick(+1)}>+1</button>
+    </form>
+  )
+
+
+}
+
+function CustomAdder({count, setCount}){
+  const [inputValue, setInputValue] = useState("");
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function handleAdd() {
+    let value = parseInt(inputValue, 10);
+    if (!isNaN(value)) {
+      setCount(count + value);
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (event.key == "Enter") {
+      handleAdd();
+      event.preventDefault();
+    }
+  }
+
+  return(
+    <form>
+      <h4>Add: </h4>
+      <input onChange={handleInputChange} onKeyDown={handleKeyDown} placeholder="..."></input>
+    </form>
+  )
+}
